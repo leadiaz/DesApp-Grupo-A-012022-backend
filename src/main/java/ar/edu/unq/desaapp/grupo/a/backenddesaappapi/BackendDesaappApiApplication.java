f8a110f1e9bdd4af7fec67b13a203560c8ca84d7
Leandro diaz <ldiaz@finnegans.com.ar>
2022-04-05 16:58:28 -0300

    starting project

---
+++ src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/BackendDesaappApiApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desaapp.grupo.a.backenddesaappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDesaappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDesaappApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/BackendDesaappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desaapp.grupo.a.backenddesaappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDesaappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
