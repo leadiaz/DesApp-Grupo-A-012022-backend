461bb3a8f2c0f168415d85d7697421d25e475aab
Leandro diaz <ldiaz@finnegans.com.ar>
2022-04-07 21:20:32 -0300

    testing travis

--- src/test/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/BackendDesaappApiApplicationTests.java
+++ src/test/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/BackendDesaappApiApplicationTests.java
@@ -2,12 +2,17 @@ package ar.edu.unq.desaapp.grupo.a.backenddesaappapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class BackendDesaappApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void test(){
+		assert true;
 	}
 
 }
