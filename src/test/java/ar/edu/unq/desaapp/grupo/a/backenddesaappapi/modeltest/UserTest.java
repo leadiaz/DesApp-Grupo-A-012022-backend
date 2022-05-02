4b44cd1ff07f6f8c9782b5b59253c69d2f851a31
Leandro diaz <ldiaz@finnegans.com.ar>
2022-05-02 00:46:11 -0300

    user model test

--- src/test/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/modeltest/UserTest.java
+++ src/test/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/modeltest/UserTest.java
@@ -1,6 +1,7 @@
 package ar.edu.unq.desaapp.grupo.a.backenddesaappapi.modeltest;
 
 import ar.edu.unq.desaapp.grupo.a.backenddesaappapi.model.User;
+import ar.edu.unq.desaapp.grupo.a.backenddesaappapi.util.UserRegisterChecker;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,11 +25,34 @@ public class UserTest {
         Assertions.assertEquals(user.getName(), "Leandro");
     }
     @Test
-    void minLengthNameTest(){
-        Assertions.assertTrue(user.getName().length() >= 3);
+    void minAndMaxLengthNameTest(){
+        Assertions.assertTrue(UserRegisterChecker.hasMinMaxCharacterNumber(3, 30, user.getName()));
     }
     @Test
-    void maxLengthNameTest(){
-        Assertions.assertTrue(user.getName().length() <= 30);
+    void minAndMaxLengthSurnameTest(){
+        Assertions.assertTrue(UserRegisterChecker.hasMinMaxCharacterNumber(3, 30, user.getName()));
     }
+    @Test
+    void correctEmailFormatTest(){
+        Assertions.assertTrue(UserRegisterChecker.hasEmailFormat(user.getEmail()));
+    }
+    @Test
+    void minAndMaxLengthAddressTest(){
+        Assertions.assertTrue(UserRegisterChecker.hasMinMaxCharacterNumber(10, 30, user.getAddress()));
+    }
+
+    @Test
+    //at least 1 lowercase, 1 uppercase, 1 special character and min 6
+    void validPasswordTest(){
+        Assertions.assertTrue(UserRegisterChecker.isValidPassword(user.getPassword()));
+    }
+    @Test
+    void validCVUTest(){
+        Assertions.assertTrue(UserRegisterChecker.hasMinMaxCharacterNumber(22, 22, user.getCvu()));
+    }
+    @Test
+    void validCriptoAssetAddressTest(){
+        Assertions.assertTrue(UserRegisterChecker.hasMinMaxCharacterNumber(8,8, user.getCryptoAssetAddress()));
+    }
+
 }
