c50435602d29f959b0fd483c8cce7c8230158727
EricArnez <ericarnez@gmail.com>
2022-05-03 17:33:55 -0300

    agregado punto 5 menos lo que es persistencia

--- src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/model/TransactionIntention.java
+++ src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/model/TransactionIntention.java
@@ -1,5 +1,7 @@
 package ar.edu.unq.desaapp.grupo.a.backenddesaappapi.model;
 
+import java.time.format.DateTimeFormatter;
+
 public class TransactionIntention {
 
     public enum OperationEnum {
@@ -14,6 +16,9 @@ public class TransactionIntention {
     private String userName;
     private OperationEnum operation;
 
+    private String DateOfTransaction;
+    private String timeOfTransaction;
+
     public TransactionIntention(CryptoAsset cryptoAsset, float amount, float price, float priceInArgentinianPesos, String userName, OperationEnum operation) {
         this.cryptoAsset = cryptoAsset;
         this.amount = amount;
@@ -21,6 +26,9 @@ public class TransactionIntention {
         this.priceInArgentinianPesos = priceInArgentinianPesos;
         this.userName = userName;
         this.operation = operation;
+
+        this.DateOfTransaction = java.time.LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
+        this.timeOfTransaction = java.time.LocalTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
     }
 }
 
--- src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/util/TransactionsIntentionHandler.java
+++ src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/util/TransactionsIntentionHandler.java
@@ -3,6 +3,10 @@ package ar.edu.unq.desaapp.grupo.a.backenddesaappapi.util;
 import ar.edu.unq.desaapp.grupo.a.backenddesaappapi.model.CryptoAsset;
 import ar.edu.unq.desaapp.grupo.a.backenddesaappapi.model.TransactionIntention;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TransactionsIntentionHandler {
 
     public static void createSaleTransactionIntention(CryptoAsset cryptoAsset, float amount, float price, float priceInArgentinianPesos, String userName) {
@@ -18,5 +22,11 @@ public class TransactionsIntentionHandler {
 
         //todo persistance transactionBuyIntention
 
+    }
+
+    public List<TransactionIntention> getActiveTransactionIntentionsOfUser(String userID, TransactionIntention.OperationEnum operation){
+        List<TransactionIntention> AlltransactionIntentions = new ArrayList<TransactionIntention>();
+        // todo retrieve data from persistance
+        return AlltransactionIntentions;
     }
 }
