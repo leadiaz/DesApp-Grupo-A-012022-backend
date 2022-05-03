14623db7d7dcdd57dcdc7968711381e7b8837858
EricArnez <ericarnez@gmail.com>
2022-05-03 17:18:48 -0300

    small fix

--- src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/util/TransactionsIntentionHandler.java
+++ src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/util/TransactionsIntentionHandler.java
@@ -5,16 +5,21 @@ import ar.edu.unq.desaapp.grupo.a.backenddesaappapi.model.TransactionIntention;
 
 public class TransactionsIntentionHandler {
 
-    public static void createSaleTransactionIntention(CryptoAsset cryptoAsset, float amount, float price, float priceInArgentinianPesos, String userName, TransactionIntention){
-        TransactionIntention transactionSaleIntention = new TransactionIntention
-                        (cryptoAsset, amount, price,priceInArgentinianPesos,userName, TransactionIntention.OperationEnum.Venta);
+
+    public TransactionsIntentionHandler() {
+    }
+
+    public static void createSaleTransactionIntention(CryptoAsset cryptoAsset, float amount, float price, float priceInArgentinianPesos, String userName) {
+        TransactionIntention transactionSaleIntention = new TransactionIntention(cryptoAsset, amount, price,priceInArgentinianPesos,userName, TransactionIntention.OperationEnum.Venta);
+
         //todo persistance transactionSaleIntention
 
     }
 
-    public static void createBuyTransactionIntention(CryptoAsset cryptoAsset, float amount, float price, float priceInArgentinianPesos, String userName, TransactionIntention){
+    public static void createBuyTransactionIntention(CryptoAsset cryptoAsset, float amount, float price, float priceInArgentinianPesos, String userName) {
         TransactionIntention transactionBuyIntention = new TransactionIntention
                 (cryptoAsset, amount, price,priceInArgentinianPesos,userName, TransactionIntention.OperationEnum.Compra);
+
         //todo persistance transactionBuyIntention
 
     }
