0ae74ff911748f927c00348f3e05e2847ac73950
EricArnez <ericarnez@gmail.com>
2022-05-03 17:20:33 -0300

    small refactor

--- src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/util/TransactionsIntentionHandler.java
+++ src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/util/TransactionsIntentionHandler.java
@@ -5,20 +5,16 @@ import ar.edu.unq.desaapp.grupo.a.backenddesaappapi.model.TransactionIntention;
 
 public class TransactionsIntentionHandler {
 
-
-    public TransactionsIntentionHandler() {
-    }
-
     public static void createSaleTransactionIntention(CryptoAsset cryptoAsset, float amount, float price, float priceInArgentinianPesos, String userName) {
-        TransactionIntention transactionSaleIntention = new TransactionIntention(cryptoAsset, amount, price,priceInArgentinianPesos,userName, TransactionIntention.OperationEnum.Venta);
+        TransactionIntention transactionSaleIntention =
+                new TransactionIntention(cryptoAsset, amount, price,priceInArgentinianPesos,userName, TransactionIntention.OperationEnum.Venta);
 
         //todo persistance transactionSaleIntention
-
     }
 
     public static void createBuyTransactionIntention(CryptoAsset cryptoAsset, float amount, float price, float priceInArgentinianPesos, String userName) {
-        TransactionIntention transactionBuyIntention = new TransactionIntention
-                (cryptoAsset, amount, price,priceInArgentinianPesos,userName, TransactionIntention.OperationEnum.Compra);
+        TransactionIntention transactionBuyIntention =
+                new TransactionIntention(cryptoAsset, amount, price,priceInArgentinianPesos,userName, TransactionIntention.OperationEnum.Compra);
 
         //todo persistance transactionBuyIntention
 
