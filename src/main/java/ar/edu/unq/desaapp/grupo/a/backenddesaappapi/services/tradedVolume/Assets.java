2d3d753955c011c2eb8ae741c25df1645cbf484f
EricArnez <ericarnez@gmail.com>
2022-06-14 17:59:26 -0300

    punto 7 en proceso

---
+++ src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/services/tradedVolume/Assets.java
@@ -0,0 +1,8 @@
+package ar.edu.unq.desaapp.grupo.a.backenddesaappapi.services.tradedVolume;
+
+import ar.edu.unq.desaapp.grupo.a.backenddesaappapi.model.CryptoAsset;
+
+public class Assets {
+    public Assets(CryptoAsset cryptoAsset, float totalNominalAmountCryptoAsset, float currentValueCryptoAsset, float currentValueCryptoAssetInArgentinianPesos) {
+    }
+}
---
+++ src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/services/tradedVolume/TradedVolume.java
@@ -0,0 +1,11 @@
+package ar.edu.unq.desaapp.grupo.a.backenddesaappapi.services.tradedVolume;
+
+import ar.edu.unq.desaapp.grupo.a.backenddesaappapi.model.user.User;
+
+import java.time.LocalDate;
+
+public class TradedVolume {
+
+    public TradedVolume(User userAsking, LocalDate now, float totalTradedUSD, float totalTradedArgentinianPesos, Assets asset) {
+    }
+}
