770509751eb5dbaf359685a81a3c508ced28f214
Leandro diaz <ldiaz@finnegans.com.ar>
2022-06-28 13:41:21 -0300

    add logger

--- src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/BackendDesaappApiApplication.java
+++ src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/BackendDesaappApiApplication.java
@@ -1,10 +1,14 @@
 package ar.edu.unq.desaapp.grupo.a.backenddesaappapi;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendDesaappApiApplication {
+
+	public static Logger logger = LogManager.getLogger(BackendDesaappApiApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendDesaappApiApplication.class, args);
