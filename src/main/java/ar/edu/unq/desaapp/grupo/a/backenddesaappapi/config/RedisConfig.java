acbea329e779bddc7caa2dc710d8d3275f25fdf8
Leandro diaz <ldiaz@finnegans.com.ar>
2022-07-04 18:02:03 -0300

    redis host heroku

--- src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/config/RedisConfig.java
+++ src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package ar.edu.unq.desaapp.grupo.a.backenddesaappapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -12,11 +13,15 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
+    @Value("${redis.host}")
+    private String host;
+    @Value("${redis.port}")
+    private int port;
     @Bean
     public JedisConnectionFactory connectionFactory(){
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName("localhost");
-        configuration.setPort(6379);
+        configuration.setHostName(host);
+        configuration.setPort(port);
         return new JedisConnectionFactory(configuration);
     }
     @Bean
