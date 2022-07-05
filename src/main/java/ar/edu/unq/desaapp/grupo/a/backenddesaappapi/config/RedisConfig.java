52faec61b7fdb2d716cdb299c4e8d5906a407faf
Leandro diaz <ldiaz@finnegans.com.ar>
2022-07-05 13:18:28 -0300

    redis host heroku

--- src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/config/RedisConfig.java
+++ src/main/java/ar/edu/unq/desaapp/grupo/a/backenddesaappapi/config/RedisConfig.java
@@ -22,9 +22,9 @@ public class RedisConfig {
     @Bean
     public JedisConnectionFactory connectionFactory(){
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-//        configuration.setHostName("localhost");
-//        configuration.setPassword(RedisPassword.of("pc4e91c60e50c977987ea31a8fbcce19797190cee78481c2345f709cfa024ad65"));
-//        configuration.setPort(6379);
+        configuration.setHostName("ec2-107-22-43-200.compute-1.amazonaws.com");
+        configuration.setPassword(RedisPassword.of("pc4e91c60e50c977987ea31a8fbcce19797190cee78481c2345f709cfa024ad65"));
+        configuration.setPort(11470);
         return new JedisConnectionFactory(configuration);
     }
     @Bean
